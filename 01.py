064bb2011618aabaf5e0c4153638389d7b73c576
Amirabdvalipour1980 <amirabdevalipour@gmail.com>
2025-07-28 07:19:00 -0700

    commit 1

---
+++ 01.py
@@ -0,0 +1,19 @@
+import turtle
+n = 5
+t = turtle.Turtle()
+
+if n == 5:
+
+    t.forward(100)
+    t.left(72)
+    t.forward(100)
+    t.left(72)
+    t.forward(100)
+    t.left(72)
+    t.forward(100)
+    t.left(72)
+    t.forward(100)
+    t.left(72)
+
+
+turtle.mainloop()
