7a41965e6f80f4c8d95800cd7cab96e40d21b45e
Amirabdvalipour1980 <amirabdevalipour@gmail.com>
2025-07-28 09:04:11 -0700

    sen

---
+++ 05.py
@@ -0,0 +1,2 @@
+for h in range(0, 101, 5):
+    print(h)
---
+++ 06.py
@@ -0,0 +1,12 @@
+for h in range(10):
+    a  = int(input('age: '))
+    if a < 10:
+        print("kodak")
+    elif a < 18:
+        print("nojavan")
+    elif a < 30:
+        print("javan")
+    elif a < 60:
+        print("miansal")
+    elif a < 100:
+        print("pir")
