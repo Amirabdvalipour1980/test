08ebb15d857af88c2e6d03a09a0c276f5bc93792
Amirabdvalipour1980 <amirabdevalipour@gmail.com>
2025-07-28 09:53:27 -0700

    adad

---
+++ 07.py
@@ -0,0 +1,13 @@
+import random
+a = random.randint(0 ,100)
+print()
+for h in range(5):
+    m = int(input("number: "))
+    if a > m:
+        print("adad shoma kochek tar ast!")
+    elif a < m:
+        print("ada shoma bosorg tar ast!")
+    elif a == m:
+        print("to bordi")
+    else:
+        print("to bachty")
