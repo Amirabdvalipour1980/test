5d2092e91c8e6162c95d6676085ce7c9f16247c2
Amirabdvalipour1980 <amirabdevalipour@gmail.com>
2025-08-01 06:01:15 -0700

    258963lkopijjkkjjkkjjkkk

--- 09.py
+++ 09.py
@@ -1,6 +1,6 @@
-# for h in range(1 ,10):
-#     print("*" , h+1)
-for i in range(1,10):
-   for j in range(1,i):
-      print("*",end="")
-   print("/n")
+for h in range(1 ,10):
+     print("*" * h)
+# for i in range(1,10):
+#     for j in range(1,i):
+#        print("*",end="")
+#     print()
--- 10.py
+++ 10.py
@@ -1,37 +1,10 @@
-inputn = int(input("number:"))
-if inputn <= 1:
-    print("adad aval nist")
-elif 
 
-# if h % 2 == 0:
-#    print("adad aval ")
-# elif h % 3 == 0:
-#    print("adad aval")
-# elif h % 4 == 0:
-#    print("adad aval ")
-# elif h % 5 == 0:
-#    print("adad aval 000000000")
-# elif h % 6 == 0:
-#    print("adad aval ")
-# elif h % 7 == 0:
-#    print("adad aval")
-# elif h % 8 == 0:
-#    print("adad aval ")
-# elif h % 9 == 0:
-#    print("adad aval")
-# elif h % 2 != 0:
-#    print("adad aval nist")
-# elif h % 3 != 0:
-#    print("adad aval nist")
-# elif h % 4 != 0:
-#    print("adad aval nist")
-# elif h % 5 != 0:
-#    print("adad aval ")
-# elif h % 6 != 0:
-#    print("adad aval nist")
-# elif h % 7 != 0:
-#    print("adad aval nist")
-# elif h % 8 != 0:
-#    print("adad aval nist")
-# elif h % 9 != 0:
-#    print("adad aval nist")
+
+inputn = int(input("number:"))
+n = 0
+for h in range(1 , inputn+1):
+    
+    if inputn % h ==0:
+        n = n+1
+if n == 2:
+    print("adad aval")
