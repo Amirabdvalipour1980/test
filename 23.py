5aadac46b3a3ca1cf723efa545162060272a35e1
Amirabdvalipour1980 <amirabdevalipour@gmail.com>
2025-08-11 09:21:35 -0700

    1425

---
+++ 23.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.geometry('800x600')
+window.title('تایمر')
+window.after
+
+def on_click():
+    l1.config(text='on')
+def off_click():
+    l1.config(text='off')
+
+l1 = tk.Label(window, text='')
+b1 = tk.Button(window, text='start', width=15, height=5,command=off_click)
+b2 = tk.Button(window, text='stop', width=15, height=5,command=on_click)
+b3 = tk.Button(window, text='reset', width=15, height=5,command=on_click)
+
+b1.pack()
+b2.pack()
+b3.pack()
+l1.pack()
+
+window.mainloop()
