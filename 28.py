34739bf33c37fc87d88c94e5ab6bcb434cac5164
Amirabdvalipour1980 <amirabdevalipour@gmail.com>
2025-08-18 13:02:39 -0700

    415263

--- 28.py
+++ 28.py
@@ -4,20 +4,18 @@ from tkinter import messagebox
 window = tk.Tk()
 window.geometry('500x600')
 
-
-adad = int
-kalame = str
 # def on_click():
 #     messagebox.showinfo('in yek info ast', 'hello ' )
 # def om_click():
 #     messagebox.showwarning('in yek warning ast', '253625')
 #     messagebox.showerror('in yek error ast', 'hello ' )
 e1 = tk.Entry(window, width=10, font=('Tahoma', 25))
+
 def on_click():
-   if e1 == adad:
-        messagebox.showinfo('in yek info ast', 'hello ' )
-   elif e1 == kalame:
-        messagebox.showwarning('in yek warning ast', '253625')
+   if e1.get() == '':
+        messagebox.showinfo('in yek info ast', 'مشخصات کامل خود را وارد کنید' )
+   else:
+        messagebox.showwarning('in yek warning ast', 'شما وارد شدید')
 
 
 b1 = tk.Button(window, padx=10, pady=10, font=('Tahoma', 20), text='click here', command=on_click)
