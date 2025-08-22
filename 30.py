2530456e12015cf567a9178836ac981e8df1419e
Amirabdvalipour1980 <amirabdevalipour@gmail.com>
2025-08-22 11:09:36 -0700

    1233333336666

--- 30.py
+++ 30.py
@@ -4,7 +4,7 @@ window = tk.Tk()
 window.geometry('500x600')
 
 def show_value(val):
-    window.config(bg='{red.get():02x}{green.get():02x}{blue.get():02x}')
+    window.config(bg=f'{red.get():02x}{green.get():02x}{blue.get():02x}')
 
 red = tk.Scale(window, from_=0, to=255, orient='horizontal', length=200, command=show_value)
 red.pack()
