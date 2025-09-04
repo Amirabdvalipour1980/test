7b015fbd10e71096aaddd910a98947787f7ca1fe
Amirabdvalipour1980 <amirabdevalipour@gmail.com>
2025-09-04 03:48:07 -0700

    11228

--- 38.py
+++ 38.py
@@ -10,8 +10,8 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     for (x, y, h, w) in faces:
-        cv2.rectangle(frame, (x, y),(100, 100),80, (0, 255, 0), 2)
-        # cv2.circle(frame,80,(x, y),(x+w, y+h), (0, 0, 255), 3)
+        # cv2.rectangle(frame, (x, y), (0, 255, 0), 2)
+        cv2.circle(frame,(x+w//2, y+h//2),w//2, (0, 0, 255), 3)
     cv2.imshow('webcam main frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- 39.py
+++ 39.py
@@ -13,4 +13,4 @@ while True:
     cv2.imshow('webcam mask',mask)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    cv2.destroyAllWindows()
+cv2.destroyAllWindows()
